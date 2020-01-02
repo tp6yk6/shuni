8c593dd9814778432335dfe2e5adc43d168ca728
tp6yk6 <45422483+tp6yk6@users.noreply.github.com>
2020-01-02 12:25:09 +0800

    Add files via upload

--- main.go
+++ main.go
@@ -54,7 +54,8 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 				if err != nil {
 					log.Println("Quota err:", err)
 				}
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OK! remain message:"+strconv.FormatInt(quota.Value, 10))).Do(); err != nil {
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OK! remain message:"+strconv.FormatInt(quota.Value, 10))).Do();
+				 err != nil {
 					log.Print(err)
 				}
 			}
